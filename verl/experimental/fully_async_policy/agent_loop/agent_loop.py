8fdd6a53e26c461f616e58f8b5c4a4e3a700542d
Xuan Chen <59129325+XChen-Zero@users.noreply.github.com>
2026-01-08 20:46:45 +0800

    Fix async agent loop defaults

--- verl/experimental/fully_async_policy/agent_loop/agent_loop.py
+++ verl/experimental/fully_async_policy/agent_loop/agent_loop.py
@@ -113,7 +113,8 @@ class FullyAsyncAgentLoopWorker(AgentLoopWorker):
 
         # by default, we assume it's a single turn agent
         if "agent_name" not in batch.non_tensor_batch:
-            batch.non_tensor_batch["agent_name"] = np.array(["single_turn_agent"] * len(batch), dtype=object)
+            default_agent_loop = config.agent.default_agent_loop
+            batch.non_tensor_batch["agent_name"] = np.array([default_agent_loop] * len(batch), dtype=object)
 
         if "index" in batch.non_tensor_batch:
             index = batch.non_tensor_batch["index"]
@@ -186,6 +187,8 @@ class FullyAsyncAgentLoopWorker(AgentLoopWorker):
                     server_manager=self.server_manager,
                     tokenizer=self.tokenizer,
                     processor=self.processor,
+                    dataset_cls=self.dataset_cls,
+                    dataset_config=self.config.data,
                 )
                 output: AgentLoopOutput = await agent_loop.run(
                     sampling_params, cancellation_event=self.cancellation_event, **kwargs
